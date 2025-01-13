d9cdacfada9a537d6b4fb9b0f5cca8a1e71a3b85
Abhishek Singh <abhishekrathorerispurpose@gmail.com>
2025-01-13 16:44:09 +0530

    Removed pycache

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/create_post")
-async def create_post(payload: dict = Body(...)):
+async def create_post(payload: dict = Body(...)): 
+# Fetching body from request object and converting it to dictionary and assigning it to payload
     return {"post": payload}
+
+
+@app.post("/create_post")
+async def create_post(payload: dict = Body(...)): 
+    # Fetching body from request object and converting it to dictionary and assigning it to payload
+    return {f"title : {payload['title']} content : {payload['content']}"}
